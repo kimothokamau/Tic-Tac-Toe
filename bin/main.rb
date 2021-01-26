dccf0d809e6a351a981401ffdda91b697fe252c5
Tasheka Hamilton <tasheka.tech@gmail.com>
2021-01-26 14:36:18 -0500

    Correct winning

---
+++ bin/main.rb
@@ -0,0 +1,64 @@
+# !/usr/bin/env ruby
+
+require_relative './../lib/validation'
+require_relative './../lib/win_combinations'
+
+$board = %w[1 2 3 4 5 6 7 8 9]
+
+def initialize_board
+  puts 'Enter an available number from 1-9 to select the corresponding slot on the game board.'
+  puts " #{$board[0]} | #{$board[1]} | #{$board[2]} "
+  puts ' ---------- '
+  puts " #{$board[3]} | #{$board[4]} | #{$board[5]} "
+  puts ' ---------- '
+  puts " #{$board[6]} | #{$board[7]} | #{$board[8]} "
+end
+
+$turn = 1
+
+$win = false
+
+player_x = []
+player_o = []
+
+puts 'Welcome! You are Player X. Press any key to move on to the next player.'
+gets.chomp
+
+puts 'Welcome! You are Player O. Press any key to begin the game.'
+gets.chomp
+
+game_playing = true
+
+while game_playing
+  if $turn.odd?
+    puts "It's your turn again Player X."
+    puts 'Select the slot to place your X.'
+    initialize_board
+    slot_selected = gets.chomp.to_i
+    player_x.push(slot_selected)
+    $turn += 1
+    val = Validation.new
+    val.next_turn(slot_selected, 'X')
+    win = WinCombinations.new
+    win.win_combos(player_x, 'Player X')
+
+  elsif $turn.even?
+    puts "It's your turn again Player O."
+    puts 'Select the slot to place your O.'
+    initialize_board
+    slot_selected = gets.chomp.to_i
+    player_o.push(slot_selected)
+    $turn += 1
+    val = Validation.new
+    val.next_turn(slot_selected, 'O')
+    win = WinCombinations.new
+    win.win_combos(player_o, 'Player O')
+  end
+  if $win == true
+    game_playing = false
+    puts "Congratulations #{$winner}! You are the winner!"
+  elsif $turn > 9 && $win == false
+    game_playing = false
+    puts "It's a draw. Better luck next time guys."
+  end
+end
--- lib/win_combinations.rb
+++ lib/win_combinations.rb
@@ -4,8 +4,8 @@ class WinCombinations
   def win_combos(slot, player)
     possible_wins = [[1, 2, 3], [1, 5, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [3, 5, 7], [4, 5, 6], [7, 8, 9]]
     if slot.length >= 3
-      check_array = []
       possible_wins.each do |arr|
+        check_array = []
         arr.each do |space|
           check_array.push(space) if slot.include?(space)
           next unless possible_wins.include?(check_array)
