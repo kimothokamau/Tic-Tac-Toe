b26cd73b2ef95ad74663d8a6edf2363b178274d7
Tasheka Hamilton <tasheka.tech@gmail.com>
2021-01-27 07:03:48 -0500

    Correct additioal linter

--- lib/validation.rb
+++ lib/validation.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Style/RedundantReturn
 class Validation
   $available_slots = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
@@ -7,10 +8,12 @@ class Validation
       return 'Invalid Input. Select a number between 1 and 9.'
     elsif (!$available_slots.include? selected) || (selected < 1 || selected > 9)
       $turn -= 1
-      return 'That slot is not available. Select an available slot.'  
+      return 'That slot is not available. Select an available slot.'
     else
       $available_slots.delete(selected)
       $board[selected - 1] = x_or_o
     end
   end
 end
+
+# rubocop:enable Style/RedundantReturn
