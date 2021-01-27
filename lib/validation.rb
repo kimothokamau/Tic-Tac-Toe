c67301903e14eb16b45d61aaf1223ab5ecf14a80
Tasheka Hamilton <tasheka.tech@gmail.com>
2021-01-27 06:59:25 -0500

    Correct linter

--- lib/validation.rb
+++ lib/validation.rb
@@ -3,11 +3,11 @@ class Validation
 
   def next_turn(selected, x_or_o)
     if selected < 1 || selected > 9
+      $turn -= 1
       return 'Invalid Input. Select a number between 1 and 9.'
-      $turn -= 1
     elsif (!$available_slots.include? selected) || (selected < 1 || selected > 9)
-      return 'That slot is not available. Select an available slot.'
       $turn -= 1
+      return 'That slot is not available. Select an available slot.'  
     else
       $available_slots.delete(selected)
       $board[selected - 1] = x_or_o
