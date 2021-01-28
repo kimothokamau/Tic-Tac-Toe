b739db823d8bfa2250c75752c76451b9048a6c78
Tasheka Hamilton <tasheka.tech@gmail.com>
2021-01-28 18:38:21 -0500

    Add  validation and win combo rspec

--- lib/validation.rb
+++ lib/validation.rb
@@ -4,10 +4,10 @@ class Validation
 
   def next_turn(selected, x_or_o)
     if selected < 1 || selected > 9
-      $turn -= 1
+     # $turn -= 1
       return 'Invalid Input. Select a number between 1 and 9.'
     elsif (!$available_slots.include? selected) || (selected < 1 || selected > 9)
-      $turn -= 1
+     # $turn -= 1
       return 'That slot is not available. Select an available slot.'
     else
       $available_slots.delete(selected)
---
+++ spec/tic_tac_toe_spec.rb
@@ -0,0 +1,36 @@
+require_relative '../lib/validation'
+require_relative '../lib/win_combinations'
+
+
+describe Validation do
+    describe '#next_turn' do
+      it 'compares the selection with a number less than 1' do
+        validation = Validation.new
+        expect(p(validation.next_turn(0, "x"))).to eql('Invalid Input. Select a number between 1 and 9.')
+    end
+
+    it 'compares the selection with a number greater than  9' do
+      validation = Validation.new
+      expect(p(validation.next_turn(12, "x"))).to eql('Invalid Input. Select a number between 1 and 9.')
+  end
+  end
+end
+
+describe WinCombinations do
+  describe '#win_combos' do
+    it 'compares if player x is the winner' do
+      win_combinations = WinCombinations.new
+      expect(p(win_combinations.win_combos([1, 2, 3], [1, 2, 3, 4, 5, 6, 7], "Player X"))).to eql("Congratulations Player X! You are the winner!")
+  end
+
+  it 'compares if player y is the winner' do
+    win_combinations = WinCombinations.new
+    expect(p(win_combinations.win_combos([3, 6, 9], [1, 2, 3, 4, 5, 6, 7, 8], "Player Y"))).to eql("Congratulations Player Y! You are the winner!")
+    end
+
+  it 'compares if nobody won and the game was drawn' do
+    win_combinations = WinCombinations.new
+    expect(p(win_combinations.win_combos([1, 2, 5], [1, 2, 3, 4, 5, 6, 7, 8, 9], "Player X"))).to eql("It's a draw. Better luck next time guys.")
+    end
+end
+end
